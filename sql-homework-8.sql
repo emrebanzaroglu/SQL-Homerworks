cfbfb06948088c0c0f09907fed62c23f492de011
EMRE BANZAROĞLU <108731430+emrebanzaroglu@users.noreply.github.com>
2022-09-29 19:05:57 +0300

    Update sql-homework-8.sql

--- sql-homework-8.sql
+++ sql-homework-8.sql
@@ -1,7 +1,7 @@
---1) test veritabanýnýzda employee isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE, email VARCHAR(100) olan bir tablo oluþturalým.
---2) Oluþturduðumuz employee tablosuna 'Mockaroo' servisini kullanarak 50 adet veri ekleyelim.
---3) Sütunlarýn her birine göre diðer sütunlarý güncelleyecek 5 adet UPDATE iþlemi yapalým.
---4) Sütunlarýn her birine göre ilgili satýrý silecek 5 adet DELETE iþlemi yapalým.
+--1) test veritabanınızda employee isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE, email VARCHAR(100) olan bir tablo oluşturalım.
+--2) Oluşturduğumuz employee tablosuna 'Mockaroo' servisini kullanarak 50 adet veri ekleyelim.
+--3) Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.
+--4) Sütunların her birine göre ilgili satırı silecek 5 adet DELETE işlemi yapalım.
 
 
 1) CREATE TABLE employee (
