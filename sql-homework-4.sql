13cdd9e86460f927500fdcd3487428b34a09aa0b
EMRE BANZAROĞLU <108731430+emrebanzaroglu@users.noreply.github.com>
2022-09-29 18:42:20 +0300

    Update sql-homework-4.sql

--- sql-homework-4.sql
+++ sql-homework-4.sql
@@ -1,10 +1,10 @@
---Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+--AÅŸaÄŸÄ±daki sorgu senaryolarÄ±nÄ± dvdrental Ã¶rnek veri tabanÄ± Ã¼zerinden gerÃ§ekleÅŸtiriniz.
 
---1) film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
---2) film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
---3) film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
---4) country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
---5) city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+--1) film tablosunda bulunan replacement_cost sÃ¼tununda bulunan birbirinden farklÄ± deÄŸerleri sÄ±ralayÄ±nÄ±z.
+--2) film tablosunda bulunan replacement_cost sÃ¼tununda birbirinden farklÄ± kaÃ§ tane veri vardÄ±r?
+--3) film tablosunda bulunan film isimlerinde (title) kaÃ§ tanesini T karakteri ile baÅŸlar ve aynÄ± zamanda rating 'G' ye eÅŸittir?
+--4) country tablosunda bulunan Ã¼lke isimlerinden (country) kaÃ§ tanesi 5 karakterden oluÅŸmaktadÄ±r?
+--5) city tablosundaki ÅŸehir isimlerinin kaÃ§ tanesi 'R' veya r karakteri ile biter?
 
 
 1) SELECT DISTINCT replacement_cost FROM film;
@@ -16,4 +16,4 @@
 4) SELECT COUNT(*) FROM country WHERE country LIKE '_____';
 
 5) SELECT COUNT(*) FROM city WHERE city ILIKE '%R'
-```
+
