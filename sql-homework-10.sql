367b06a97e768a50125848f157443b342111c838
EMRE BANZAROĞLU <108731430+emrebanzaroglu@users.noreply.github.com>
2022-09-29 19:13:24 +0300

    Add files via upload

---
+++ sql-homework-10.sql
@@ -0,0 +1,15 @@
+--Aþaðýdaki sorgu senaryolarýný dvdrental örnek veri tabaný üzerinden gerçekleþtiriniz.
+
+--1) city tablosu ile country tablosunda bulunan þehir (city) ve ülke (country) isimlerini birlikte görebileceðimiz LEFT JOIN sorgusunu yazýnýz.
+--2) customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceðimiz RIGHT JOIN sorgusunu yazýnýz.
+--3) customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceðimiz FULL JOIN sorgusunu yazýnýz.
+
+1) SELECT city.city,country.country FROM city
+LEFT JOIN country ON city.country_id = country.country_id;
+
+2) SELECT payment.payment_id,customer.first_name,customer.last_name FROM customer
+RIGHT JOIN payment ON payment.customer_id = customer.customer_id;
+
+3) SELECT rental.rental_id,customer.first_name,customer.last_name FROM customer
+FULL JOIN rental  ON rental.customer_id = customer.customer_id;
+```
